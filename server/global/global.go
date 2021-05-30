4fe2d05e7c5f23e8dc5cce8289e525841a51a3f2
liuhuan <184555556@qq.com>
2021-05-30 23:38:39 +0800

    增加redis、将优雅关机摘出来

--- server/global/global.go
+++ server/global/global.go
@@ -2,15 +2,16 @@ package global
 
 import (
 	"gin-element-admin/config"
+	"github.com/go-redis/redis"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
 
 var (
-	GEA_DB *gorm.DB
-	//GEA_REDIS *redis.Client
+	GEA_DB     *gorm.DB
 	GEA_LOG    *zap.Logger
 	GEA_VP     *viper.Viper
 	GEA_CONFIG config.Server
+	GEA_REDIS  *redis.Client
 )
---
+++ server/initialize/redis.go
@@ -0,0 +1,23 @@
+package initialize
+
+import (
+	"gin-element-admin/global"
+	"github.com/go-redis/redis"
+	"go.uber.org/zap"
+)
+
+func Redis() {
+	redisCfg := global.GEA_CONFIG.Redis
+	client := redis.NewClient(&redis.Options{
+		Addr:     redisCfg.Addr,
+		Password: redisCfg.Password, // no password set
+		DB:       redisCfg.DB,       // use default DB
+	})
+	pong, err := client.Ping().Result()
+	if err != nil {
+		global.GEA_LOG.Error("redis connect ping failed, err:", zap.Any("err", err))
+	} else {
+		global.GEA_LOG.Info("redis connect ping response:", zap.String("pong", pong))
+		global.GEA_REDIS = client
+	}
+}
